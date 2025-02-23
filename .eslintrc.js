43ef7c0fb7e096f4e7f0d1bb17190deae624be44
hachrisjordan <67759489+hachrisjordan@users.noreply.github.com>
2025-02-22 22:03:31 -0500

    3.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,3 @@
 module.exports = {
-  extends: [
-    'next/core-web-vitals',
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
-  rules: {
-    'no-unused-vars': 'warn',
-    'no-undef': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
-  }
-} 
+  extends: ['react-app', 'react-app/jest'],
+}; 
